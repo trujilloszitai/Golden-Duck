a36273c21434ad8797788da9edcdef7089093915
Jorgemaxii25 <jorge.maxii25@gmail.com>
2022-06-08 17:46:31 -0300

    Subo avance de página web

--- GoldenDuck-WebPage/Sources/js/indexScript.js
+++ GoldenDuck-WebPage/Sources/js/indexScript.js
@@ -11,12 +11,11 @@ $(document).ready(function(){
         }
     })
 });
-
 function irArriba(){
-    $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
+    $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },0); });
     $(window).scroll(function(){
     if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(300); }else{ $('.ir-arriba').slideUp(300); }
     });
-    $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+    $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },0); });
     if($(this).scrollTop() > 0){ $('.ir-arriba').css}
 }
