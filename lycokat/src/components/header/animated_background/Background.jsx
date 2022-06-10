debd485f8ec417cd79d6946483da46c39b0f01d8
Francisco Trujillo <trujilloszitai@gmail.com>
2022-06-10 10:22:16 -0300

    Fondo animado cambiado q

---
+++ lycokat/src/components/header/animated_background/Background.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './background.scss'
+
+const Background = () => {
+    return (
+        <div>
+            <div class="area" >
+                <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div >
+        </div>
+    )
+}
+
+export default Background
