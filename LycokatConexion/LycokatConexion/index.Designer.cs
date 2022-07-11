3e46ccf430122c81e2c29d910a15e7473b3d63e7
Fede <fedelva8@gmail.com>
2022-07-11 16:53:39 -0300

    arregle un merge en mi pc

--- LycokatConexion/LycokatConexion/index.Designer.cs
+++ LycokatConexion/LycokatConexion/index.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(163, 111);
+            this.button1.Location = new System.Drawing.Point(317, 196);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(164, 23);
             this.button1.TabIndex = 0;
