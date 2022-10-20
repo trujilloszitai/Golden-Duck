ac714c079e18c1fd8c14d31e5386798fdb1df321
JorgePeñaranda <80211041+JorgePeniaranda@users.noreply.github.com>
2022-10-20 15:39:48 -0300

    Update Paso3.js

--- src/pages/ForgotPassword/components/Paso3.js
+++ src/pages/ForgotPassword/components/Paso3.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Swal from "sweetalert2";
-import InputPasswordShowBTN from "../../../components/InputPasswordShowBTN/InputPasswordShowBTN";
+import InputPasswordShowBTN from "../../../components/InputPasswordShowBTN/inputPasswordShowBTN";
 import formFunctions from "../../../utils/formFunctions";
 
 const Paso3 = (props) => {
